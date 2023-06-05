391b33fa77ef7a9693931971f1ecda0324cd3853
benjrise <benjrise@hotmail.co.uk>
2023-06-05 17:20:36 +0100

    nvim update

--- ftplugin/tex.lua
+++ ftplugin/tex.lua
@@ -1,21 +1,21 @@
 require("user.ftutils.shared-md-tex")
 
 -- if packer_plugins["which-key.nvim"] and packer_plugins["which-key.nvim"].loaded then
-	local wk = require("which-key")
-	local opts = {
-		prefix = "",
-		buffer = 0, -- Global mappings. Specify a buffer number for buffer local mappings
-		silent = true, 
-		noremap = true, 
-		nowait = false,
-	}
+local wk = require("which-key")
+local opts = {
+   prefix = "",
+   buffer = 0, -- Global mappings. Specify a buffer number for buffer local mappings
+   silent = true, 
+   noremap = true, 
+   nowait = false,
+}
 
-	local mappings = {
-    ["<localleader>b"] = { "<cmd>VimtexCompile<CR>"            , "build" },
-    ["<localleader>v"] = { "<cmd>VimtexView<CR>"               , "view" },
-	}
+local mappings = {
+ ["<localleader>b"] = { "<cmd>VimtexCompile<CR>"            , "build" },
+ ["<localleader>v"] = { "<cmd>VimtexView<CR>"               , "view" },
+}
 
-	wk.register(mappings, opts)
+wk.register(mappings, opts)
 
 -- keyma("v", "<S-b>", ":'<,'>:norm 
 -- end
--- lua/user/ftutils/shared-md-tex.lua
+++ lua/user/ftutils/shared-md-tex.lua
@@ -1,8 +1,8 @@
 -- Share these command for only md and tex
 local opts = {
    -- buffer = true, -- Global mappings. Specify a buffer number for buffer local mappings
-   silent = true, 
-   noremap = true, 
+   silent = true,
+   noremap = true
 }
 
 vim.cmd([[setlocal wrap]])
@@ -11,5 +11,5 @@ vim.cmd([[setlocal spell]])
 -- On creation of buffer with md or tex set these keymaps
 local buf_keymap = vim.api.nvim_buf_set_keymap
 -- Undo after space rather than change from insert mode, much more useful when writing
-buf_keymap(0, "i", "<space>", "<space><C-g>u", opts) 
+buf_keymap(0, "i", "<space>", "<space><C-g>u", opts)
 
